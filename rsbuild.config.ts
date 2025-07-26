fce45816048431a302a6aa8a9ce2bfb569d8fb32
CPunisher <1343316114@qq.com>
2025-07-26 17:05:07 +0800

    feat: add icon

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,18 @@
-import { defineConfig } from "@rsbuild/core";
-import { pluginReact } from "@rsbuild/plugin-react";
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    title: 'Rspack REPL',
+    appIcon: {
+      name: 'Rspack REPL',
+      icons: [
+        {
+          src: 'public/favicon-128x128.png',
+          size: 128,
+        },
+      ],
+    },
+  },
 });
