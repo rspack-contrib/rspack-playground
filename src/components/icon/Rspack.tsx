5ea1188278e77a6bacd4e28986e3cb2d8c536b63
CPunisher <1343316114@qq.com>
2025-08-01 18:06:22 +0800

    fix: rspack logo

--- src/components/icon/Rspack.tsx
+++ src/components/icon/Rspack.tsx
@@ -1,11 +1,5 @@
 import type { JSX } from "react";
 
 export default function Rspack(props: JSX.IntrinsicElements["img"]) {
-  return (
-    <img
-      src="https://assets.rspack.rs/rspack/rspack-logo.svg"
-      alt="Rspack"
-      {...props}
-    />
-  );
+  return <img src="/rspack-logo.svg" alt="Rspack" {...props} />;
 }
