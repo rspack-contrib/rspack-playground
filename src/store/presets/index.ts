30e23bf4954f4a8a2b852eee2a04a75c220d1ede
CPunisher <1343316114@qq.com>
2025-08-20 15:13:21 +0800

    feat: preset react

--- src/store/presets/index.ts
+++ src/store/presets/index.ts
@@ -1,12 +1,17 @@
 import type { SourceFile } from "../bundler";
 import PresetBasicLibrary from "./basic-library";
 import PresetEsmSh from "./esm-sh";
+import PresetReact from "./react";
 
 export interface SourcePreset {
   name: string;
   files: SourceFile[];
 }
 
-export const presets: SourcePreset[] = [PresetBasicLibrary, PresetEsmSh];
+export const presets: SourcePreset[] = [
+  PresetBasicLibrary,
+  PresetEsmSh,
+  PresetReact,
+];
 
-export { PresetBasicLibrary, PresetEsmSh };
+export { PresetBasicLibrary, PresetEsmSh, PresetReact };
